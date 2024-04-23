28a229d711080ef4c6c62e34d6b030a61cbffa64
Edwin Hernández <edwinhdevelop@gmail.com>
2024-04-23 00:36:01 -0400

    welcome

---
+++ controllers/mainController.js
@@ -0,0 +1,4 @@
+
+export const getMain = async (req, res) => {
+  res.json({ message: "Bienvenidos" })
+}
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express"
 import dotenv from "dotenv"
 import productRoutes from "./routes/productRoutes.js"
+import mainRoutes from "./routes/mainRoutes.js"
 
 const app = express()
 app.use(express.json())
 
 dotenv.config()
 
+app.use('/', mainRoutes)
 app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 4000
